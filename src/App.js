29a8d3f0b108433f889d4a721c8e97ee15cb76d9
YudiR <yudrose13@gmail.com>
2019-07-11 22:08:44 -0400

    intrest Calculator

--- src/App.js
+++ src/App.js
@@ -17,38 +17,120 @@ class App extends React.Component {
     };
   }
 
-  calc = obj => {};
+  calc = obj => {
+    if (isNaN(obj.principal || obj.rate || obj.years)) {
+      return obj;
+    } else {
+      console.log("obj::::", obj);
+
+      obj.amount = obj.principle * (1 + (obj.rate / 100) * obj.years);
+      console.log("object amount...", obj);
+
+      return obj;
+    }
+  };
+
+  
+  add = () => {
+    console.log("hey!!!!!!!!!!")
+    let dataCopy = JSON.parse(JSON.stringify(this.state.data));
+    let newObj = {
+      principle: "",
+      rate: "",
+      years: "",
+      amount: ""
+    };
+    dataCopy.push(newObj);
+    this.setState({
+      data: dataCopy
+    });
+  };
+
+ 
+  
+  delete = (index) =>{
+    console.log("delete",index)
+    let dataCopy = JSON.parse(JSON.stringify(this.state.data))
+    dataCopy.splice(index,1) 
+    
+    
+    this.setState(
+      {
+        data: dataCopy
+      }
+    )
+  }
 
   principleChange = pData => {
     console.log(pData);
     // let tempArray = {...this.state.data[pData.index]}
     let dataCopy = JSON.parse(JSON.stringify(this.state.data));
     dataCopy[pData.index].principle = pData.text;
-
-    // tempArray.principle = pData.text
-    // tempArray = this.calc(tempArray)
-
+    
+    dataCopy[pData.index] = this.calc(dataCopy[pData.index]);
+    
     this.setState(
       {
         data: dataCopy
-        // data: {...tempArray}
-        // data: update(this.state.items, {1: {name: {$set: 'updated field name'}}})
-      },
-      () => console.log("state::::",this.state.data[0])
+      }
     );
     console.log("temp:::", dataCopy);
   };
 
+  rateChange = data => {
+    console.log(data);
+    let dataCopy = JSON.parse(JSON.stringify(this.state.data));
+    dataCopy[data.index].rate = data.text;
+
+    dataCopy[data.index] = this.calc(dataCopy[data.index]);
+
+    this.setState(
+      {
+        data: dataCopy
+      }
+    );
+  };
+
+  yearsChange = data => {
+    let dataCopy = JSON.parse(JSON.stringify(this.state.data));
+    dataCopy[data.index].years = data.text;
+
+    dataCopy[data.index] = this.calc(dataCopy[data.index]);
+
+    this.setState({
+      data: dataCopy
+    });
+  };
+
   render() {
+    let total = 0;
+
+      this.state.data.forEach(e=>{
+        console.log("E::",isNaN(e.amount));
+        
+        if(!isNaN(parseFloat(e.amount))){
+         total += parseFloat(e.amount)
+      }
+      })
+    
     return (
       <div>
         {this.state.data.map((obj, index) => (
           <Intrest
+            key={index}
             {...obj}
+            // rate={object[0].rate}
             index={index}
             principleChange={this.principleChange}
+            rateChange={this.rateChange}
+            yearsChange={this.yearsChange}
+            calc={this.calc}
+            add={this.add}
+            delete={this.delete}
           />
+         
         ))}
+        <h1>TOTAL:{total}</h1>
       </div>
     );
   }
--- src/intrest.js
+++ src/intrest.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import React from "react";
 
-const intrest = (props) => {
-    return(
-        <div>
-            <input 
-            onChange={event => {
-                props.principleChange({index:props.index, text:event.target.value})}}
-            placeholder = "principle"
-            // value={props.principle}
-            />
-        </div>
-    )
-}
+const intrest = props => {
+  return (
+    <div>
+      <input
+        onChange={event => {
+          props.principleChange({
+            index: props.index,
+            text: event.target.value
+          });
+        }}
+        placeholder="principle"
+        value={props.principle}
+        type="number"
+      />
+      <input
+        onChange={event => {
+          props.rateChange({ index: props.index, text: event.target.value });
+        }}
+        placeholder="rate"
+        value={props.rate}
+        type="number"
+      />
+      <input
+        onChange={event => {
+          props.yearsChange({ index: props.index, text: event.target.value });
+        }}
+        placeholder="years"
+        value={props.years}
+        type="number"
+      />
+      <input
+        // onChange={event => {
+        // props.yearsChange({index:props.index, text:event.target.value})}}
+        placeholder="total amount"
+        value={props.amount}
+        type="number"
+        disabled={true}
+        function={props.calc}
+      />
+      <button onClick={e => props.add()}>+</button>
+      {props.index !== 0 ? (
+        <button onClick={e => props.delete(props.index)}>-</button>
+      ) : null}
+    </div>
+  );    
+};
 
-export default intrest
+export default intrest;
